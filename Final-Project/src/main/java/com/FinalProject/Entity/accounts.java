9e224aeb5e09ee8d886d367d7f0be45d03e6a320
AswanthTKM <aswanthpgm@gmail.com>
2022-03-17 12:06:43 +0530

    commit

---
+++ Final-Project/src/main/java/com/FinalProject/Entity/accounts.java
@@ -0,0 +1,48 @@
+package com.FinalProject.Entity;
+
+public class accounts {
+	
+	private int sno;
+	private int vendorid;
+	private String subscription;
+	private int paidamount;
+	public int getSno() {
+		return sno;
+	}
+	public void setSno(int sno) {
+		this.sno = sno;
+	}
+	public int getVendorid() {
+		return vendorid;
+	}
+	public void setVendorid(int vendorid) {
+		this.vendorid = vendorid;
+	}
+	public String getSubscription() {
+		return subscription;
+	}
+	public void setSubscription(String subscription) {
+		this.subscription = subscription;
+	}
+	public int getPaidamount() {
+		return paidamount;
+	}
+	public void setPaidamount(int paidamount) {
+		this.paidamount = paidamount;
+	}
+	
+	public accounts(int sno, int vendorid, String subscription, int paidamount) {
+		super();
+		this.sno = sno;
+		this.vendorid = vendorid;
+		this.subscription = subscription;
+		this.paidamount = paidamount;
+		
+	}
+	public accounts() {
+		super();
+	}
+	
+	
+
+}
---
+++ Final-Project/src/main/java/com/FinalProject/Repo/LoginRepo.java
@@ -0,0 +1,17 @@
+package com.FinalProject.Repo;
+
+import com.FinalProject.Entity.accounts;
+import com.FinalProject.Entity.foodie;
+import com.FinalProject.Entity.vendor;
+
+public interface LoginRepo {
+	
+	public boolean validateAdmin(String username,String password);
+	public vendor validatevendor(String vendorid,String password);
+	public foodie validatefoodie(String foodieid,String password);
+    public boolean addvendor (vendor ven);
+    public boolean addfoodie (foodie fod);
+    public boolean makepayment(accounts acc);
+    
+    
+}
